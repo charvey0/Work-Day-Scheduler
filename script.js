6494dfba4517652a2c9a86f48cb50a89ecfc525d
Cameron Harvey <charvey0@gmail.com>
2021-01-23 22:30:11 -0800

    figured out how to compare times

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function init(s, e) {
-    var day = moment().format('YYYY-MM-DD');
     var slots = $("#time-slots");
     $("#currentDay").text(moment().format('dddd, MMMM Do, YYYY'));
 
@@ -9,15 +8,16 @@ function init(s, e) {
     }
 
     slots.empty();
+    var currentTime = moment().format('h:mm a');
     for (var i=0 ; i<hours.length ; i++) {
         if (hours[i]<13) {
             if (hours[i] == 12) {
-                var time = hours[i]+":00 PM";
+                var time = hours[i]+":00 pm";
             } else {
-                var time = hours[i]+":00 AM";
+                var time = hours[i]+":00 am";
             }
         } else {
-            var time = hours[i]-12+":00 PM";
+            var time = hours[i]-12+":00 pm";
         }
         var tasks = getTasks("task"+hours[i]);
         var html = "<div class='row'><div class='hour'><div value='"+hours[i]+" class='time-block col-sm-1'>";
@@ -26,9 +26,13 @@ function init(s, e) {
         html += tasks;
         html += "</textarea><div value='"+hours[i]+"' class='saveBtn'>✔</div></div>";
         slots.append(html);
+        
+        console.log("time: "+time+"   currentTime: "+currentTime);
+        console.log(time>currentTime);
     }
     
 }
+
 
 
 function getTasks(id) {
