935a5cccbd576d5d338ad8fc62a13a7eac342af2
Cameron Harvey <charvey0@gmail.com>
2021-01-23 16:55:42 -0800

    for loop to create all the times for the time slots to be displayed

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 function init() {
+    var startOfDay = 8;
+    var endOfDay = 17;
+    var day = moment().format('YYYY-MM-DD');
+    var slots = $("#time-slots");
+    
+    var hours = []
+    for (var i=startOfDay ; i < endOfDay ; i++) {
+        hours.push(day+"T"+i+":00");
+    }
+    console.log(hours);
 }
 
